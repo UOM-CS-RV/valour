3dbb22235dffd872852c01dec9aba97fe26225d9
Edward Mallia <edmallia@gmail.com>
2016-05-18 23:26:20 +0200

    initial commit

---
+++ valour.parent/valour.web/src/main/webapp/xtext-resources/generated/mode-valour.js
@@ -0,0 +1,36 @@
+define(["ace/lib/oop", "ace/mode/text", "ace/mode/text_highlight_rules"], function(oop, mText, mTextHighlightRules) {
+	var HighlightRules = function() {
+		var keywords = "action|as|belonging|by|case|catch|category|condition|controlflow|declarations|default|do|else|event|extends|extension|false|finally|for|foreach|if|import|in|index|indexed|instanceof|monitor|new|null|parallel|replicate|return|state|static|super|switch|synchronized|system|throw|to|trigger|true|try|typeof|val|var|when|where|while|with";
+		this.$rules = {
+			"start": [
+				{token: "comment", regex: "\\/\\/.*$"},
+				{token: "comment", regex: "\\/\\*", next : "comment"},
+				{token: "string", regex: '["](?:(?:\\\\.)|(?:[^"\\\\]))*?["]'},
+				{token: "string", regex: "['](?:(?:\\\\.)|(?:[^'\\\\]))*?[']"},
+				{token: "constant.numeric", regex: "[+-]?\\d+(?:(?:\\.\\d*)?(?:[eE][+-]?\\d+)?)?\\b"},
+				{token: "constant.numeric", regex: "0[xX][0-9a-fA-F]+\\b"},
+				{token: "lparen", regex: "[\\[({]"},
+				{token: "rparen", regex: "[\\])}]"},
+				{token: "keyword", regex: "\\b(?:" + keywords + ")\\b"}
+			],
+			"comment": [
+				{token: "comment", regex: ".*?\\*\\/", next : "start"},
+				{token: "comment", regex: ".+"}
+			]
+		};
+	};
+	oop.inherits(HighlightRules, mTextHighlightRules.TextHighlightRules);
+	
+	var Mode = function() {
+		this.HighlightRules = HighlightRules;
+	};
+	oop.inherits(Mode, mText.Mode);
+	Mode.prototype.$id = "xtext/valour";
+	Mode.prototype.getCompletions = function(state, session, pos, prefix) {
+		return [];
+	}
+	
+	return {
+		Mode: Mode
+	};
+});
